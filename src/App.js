74f0e8689f4b6ee3bcaa8b1015de85fed8ab3cbe
Lasith Koswatta Gamage <lasithvindu1@gmail.com>
2021-09-16 11:51:39 +1000

    Grid Progress - uncomplete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    minHeight: "100vh"
+  },
+  paper: {
+    height: "100%",
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  controlsRoot: {
+    height: "100%"
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container spacing={3} className={classes.root}>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>xs=8</Paper>
+        </Grid>
+        <Grid item xs={4} className={classes.root}>
+            <Grid item xs={8}>
+              <Paper className={classes.paper}>xs=8</Paper>
+            </Grid>
+            <Grid item xs={8}>
+              <Paper className={classes.paper}>xs=8</Paper>
+            </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
