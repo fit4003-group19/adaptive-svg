7b92426eadab2dca653fa7257958597934f34625
samthiele99 <sam.thiele1@gmail.com>
2021-09-16 21:53:02 +1000

    Made changes to the questionnaire model

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,61 +1,65 @@
-import React, {useContext} from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Questionnaire from './Questionnaire';
-import {MapContext} from '../context/MapContext';
+import React, { useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Questionnaire from "./Questionnaire";
+import { MapContext } from "../context/MapContext";
 
 const useStyles = makeStyles((theme) => ({
-    attributes: {
-        flex: 1,
-        backgroundColor: "green",
-        margin: 10,
-        display: "flex",
-        flexDirection: "column",
-      },
-    attribute: { flex: 4, backgroundColor: "white", margin: 10 },
-    buttons: { flex: 1, backgroundColor: "white", margin: 10, display:"flex", justify:"space-between", flexDirection:"row" },
-  }));
+  attributes: {
+    flex: 1,
+    backgroundColor: "green",
+    margin: 10,
+    display: "flex",
+    flexDirection: "column",
+  },
+  attribute: { flex: 4, backgroundColor: "white", margin: 10 },
+  buttons: {
+    flex: 1,
+    backgroundColor: "white",
+    margin: 10,
+    display: "flex",
+    justify: "space-between",
+    flexDirection: "row",
+  },
+}));
 
 function Controls() {
-    const classes = useStyles();
-    const {mapPanZoom, setMapPanZoom, rootFocus} = useContext(MapContext)
+  const classes = useStyles();
+  const { mapPanZoom, setMapPanZoom, rootFocus } = useContext(MapContext);
 
-    return (
-        <div className={classes.attributes}>
-            <div className={classes.attribute}>
-                <label ref={rootFocus}>
-                    Questionaire
-                    <Questionnaire/>
-                </label>
-
-            </div>
-            <div className={classes.attribute}>
-                <h1>Hey</h1>
-            </div>
-            <div className={classes.attribute}>
-                <h1>Hey</h1>
-            </div>
-            <div className={classes.buttons}>
-            {
-                mapPanZoom &&
-                <>
-                    <label>
-                        Zoom In
-                        <button onClick={() => mapPanZoom.zoomIn()}>+</button>
-                    </label>
-                    <label>
-                        Zoom Out
-                        <button onClick={() => mapPanZoom.zoomOut()}>-</button>
-                    </label>
-                    <label>
-                        Reset
-                        <button onClick={() => mapPanZoom.resetZoom()}>x</button>
-                    </label>
-                </>
-
-            }
-            </div>
-        </div>
-    )
+  return (
+    <div className={classes.attributes}>
+      <div className={classes.attribute}>
+        <label ref={rootFocus}>
+          Questionnaire
+          <Questionnaire />
+        </label>
+      </div>
+      <div className={classes.attribute}>
+        <h1>Hey</h1>
+      </div>
+      <div className={classes.attribute}>
+        <h1>Hey</h1>
+      </div>
+      <div className={classes.buttons}>
+        {mapPanZoom && (
+          <>
+            <label>
+              Zoom In
+              <button onClick={() => mapPanZoom.zoomIn()}>+</button>
+            </label>
+            <label>
+              Zoom Out
+              <button onClick={() => mapPanZoom.zoomOut()}>-</button>
+            </label>
+            <label>
+              Reset
+              <button onClick={() => mapPanZoom.resetZoom()}>x</button>
+            </label>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Controls
+export default Controls;
--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -1,27 +1,57 @@
-import React, {useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import CloseIcon from '@material-ui/icons/Close';
-import Slide from '@material-ui/core/Slide';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
+import Slide from "@material-ui/core/Slide";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    position: 'relative',
+    position: "relative",
   },
   title: {
     marginLeft: theme.spacing(2),
     flex: 1,
   },
 }));
+const RadioButtonsGroup = () => {
+  const [value, setValue] = React.useState("female");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  console.log("Binary Number", parseInt("0011"));
+
+  return (
+    <FormControl component="fieldset">
+      {/* <FormLabel component="legend">Response</FormLabel> */}
+      <RadioGroup
+        aria-label="gender"
+        name="gender1"
+        value={value}
+        onChange={handleChange}
+        row
+      >
+        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+        <FormControlLabel value="no" control={<Radio />} label="No" />
+      </RadioGroup>
+    </FormControl>
+  );
+};
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -39,19 +69,67 @@ export default function Questionnaire() {
     setOpen(false);
   };
 
+  let questions = [
+    {
+      id: 0,
+      question: "Do you experience a motor impairment?",
+      response: null,
+    },
+    {
+      id: 1,
+      question: "Do you experience a colour impairment?",
+      response: null,
+    },
+    {
+      id: 2,
+      question: "Do you have low vision?",
+      response: null,
+    },
+    {
+      id: 3,
+      question: "Do you experience total blindness?",
+      response: null,
+    },
+    {
+      id: 4,
+      question: "Do you have difficulty reading?",
+      response: null,
+    },
+    {
+      id: 5,
+      question: "Do you have difficulties operating doors?",
+      response: null,
+    },
+    {
+      id: 6,
+      question: "Do obstacles disrupt your indoor navigation?",
+      response: null,
+    },
+  ];
+
   return (
     <div>
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
         Open full-screen dialog
       </Button>
-      <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
         <AppBar className={classes.appBar}>
           <Toolbar>
-            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
               <CloseIcon />
             </IconButton>
             <Typography variant="h6" className={classes.title}>
-              Sound
+              Map Questionnaire
             </Typography>
             <Button autoFocus color="inherit" onClick={handleClose}>
               save
@@ -59,13 +137,15 @@ export default function Questionnaire() {
           </Toolbar>
         </AppBar>
         <List>
-          <ListItem button>
-            <ListItemText primary="Phone ringtone" secondary="Titania" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText primary="Default notification ringtone" secondary="Tethys" />
-          </ListItem>
+          {questions.map((question) => (
+            <>
+              <ListItem>
+                <ListItemText primary={question.question} />
+                <RadioButtonsGroup />
+              </ListItem>
+              <Divider />
+            </>
+          ))}
         </List>
       </Dialog>
     </div>
