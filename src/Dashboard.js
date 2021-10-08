ab7c5c928a97c18b59f5d269b86fee88bfef554a
Lasith Koswatta Gamage <lasithvindu1@gmail.com>
2021-10-08 15:00:17 +1100

    Migrating to Dashboard

---
+++ src/Dashboard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Dashboard.scss";
+
+import Map from "./components/map/Map";
+import Controls from "./components/controls/Controls";
+import RoomInformation from "./components/room-information/RoomInformation";
+import Options from "./components/options/Options";
+import Shortcuts from "./components/shortcuts/Shortcuts";
+import Title from "./components/title/Title";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <Title className="dashboard-title"></Title>
+      <Map className="dashboard-map"></Map>
+      <Controls className="dashboard-controls"></Controls>
+      <RoomInformation className="dashboard-description"></RoomInformation>
+      <Shortcuts className="dashboard-shortcuts"></Shortcuts>
+      <Options className="dashboard-options"></Options>
+    </div>
+  );
+};
+
+export default Dashboard;
